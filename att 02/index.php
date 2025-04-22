3a836464425977ad7f9722d68141b347fe0d8716
cauamauricio <146475246+cauamauricio@users.noreply.github.com>
2025-04-21 21:13:33 -0300

    .

---
+++ att 02/index.php
@@ -0,0 +1,34 @@
+
+<?php 
+// //boll
+// $var01 = true;
+// var_dump ( value: $var01);
+// echo "ola ola";
+// //int
+// $f = '16.5';
+// var_dump (value: $f);
+// ///float
+// $dinheiro = '1688.5';
+// var_dump (value: $dinheiro);
+// //string
+// $nome = 'caua';
+// var_dump ( value: $nome);
+
+// $largura= 20;
+// $altura= 10;
+
+// $area = $largura * $altura;
+
+// echo "a area é $area";
+//idade 
+
+$idade = 17;
+
+if ($idade) {
+
+echo "Você é muito novo para entrar no pub";
+
+}
+?>
+    
+ 
